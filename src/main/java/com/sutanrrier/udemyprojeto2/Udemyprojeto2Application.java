1875e2be129e279d34923a3c5d5f8b988807d376
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-09 17:02:15 -0300

    Project created

---
+++ src/main/java/com/sutanrrier/udemyprojeto2/Udemyprojeto2Application.java
@@ -0,0 +1,13 @@
+package com.sutanrrier.udemyprojeto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Udemyprojeto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Udemyprojeto2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sutanrrier/udemyprojeto2/Udemyprojeto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sutanrrier.udemyprojeto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Udemyprojeto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
