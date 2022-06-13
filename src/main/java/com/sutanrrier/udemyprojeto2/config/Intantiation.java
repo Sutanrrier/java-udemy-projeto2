71d39fd51afc7f2d729818527911955fa0690fe7
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-13 13:37:10 -0300

    Referenciando os posts do usuário

--- src/main/java/com/sutanrrier/udemyprojeto2/config/Intantiation.java
+++ src/main/java/com/sutanrrier/udemyprojeto2/config/Intantiation.java
@@ -29,6 +29,7 @@ public class Intantiation implements CommandLineRunner{
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 	
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
@@ -38,6 +39,9 @@ public class Intantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu viagem!", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2022"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
