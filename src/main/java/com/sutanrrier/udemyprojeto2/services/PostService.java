2bc1fe4e5751d64a88050843526640ab8ac41247
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-13 14:20:39 -0300

    Consulta simples com @Query

--- src/main/java/com/sutanrrier/udemyprojeto2/services/PostService.java
+++ src/main/java/com/sutanrrier/udemyprojeto2/services/PostService.java
@@ -1,5 +1,6 @@
 package com.sutanrrier.udemyprojeto2.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ public class PostService {
 			throw new ObjectNotFoundException("Objeto não encotrado");
 		}
 		return post.get();
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
