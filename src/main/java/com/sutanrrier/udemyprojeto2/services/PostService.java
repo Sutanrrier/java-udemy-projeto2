e93f6784a21a55712359d86fdfb50ff8dbdd76c4
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-13 13:47:49 -0300

    Obtendo um post por id

---
+++ src/main/java/com/sutanrrier/udemyprojeto2/services/PostService.java
@@ -0,0 +1,26 @@
+package com.sutanrrier.udemyprojeto2.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sutanrrier.udemyprojeto2.domain.Post;
+import com.sutanrrier.udemyprojeto2.repositories.PostRepository;
+import com.sutanrrier.udemyprojeto2.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post  = repository.findById(id);
+		if(!post.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encotrado");
+		}
+		return post.get();
+	}
+	
+}
