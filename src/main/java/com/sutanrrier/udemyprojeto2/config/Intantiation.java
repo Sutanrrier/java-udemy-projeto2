deee30232f8f425d3c1ee670270ea1b5c4156031
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-13 13:25:37 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/sutanrrier/udemyprojeto2/config/Intantiation.java
+++ src/main/java/com/sutanrrier/udemyprojeto2/config/Intantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.sutanrrier.udemyprojeto2.domain.Post;
 import com.sutanrrier.udemyprojeto2.domain.User;
+import com.sutanrrier.udemyprojeto2.dto.AuthorDTO;
 import com.sutanrrier.udemyprojeto2.repositories.PostRepository;
 import com.sutanrrier.udemyprojeto2.repositories.UserRepository;
 
@@ -34,8 +35,8 @@ public class Intantiation implements CommandLineRunner{
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu viagem!", "Vou viajar para São Paulo, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2022"), "Bom dia!", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu viagem!", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2022"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
