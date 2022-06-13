de7abc39dcc477808d73fc9fe60525b2f355f8b9
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-13 14:28:48 -0300

    Consulta simples com annotation Query

--- src/main/java/com/sutanrrier/udemyprojeto2/repositories/PostRepository.java
+++ src/main/java/com/sutanrrier/udemyprojeto2/repositories/PostRepository.java
@@ -3,12 +3,16 @@ package com.sutanrrier.udemyprojeto2.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sutanrrier.udemyprojeto2.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
